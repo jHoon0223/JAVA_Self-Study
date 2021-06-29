490d90bdbeb82368901aa78d61c99f8e4d2c17f9
jHoon0223 <aleejaehun@naver.com>
2021-06-29 19:47:58 +0900

    210629 : Anything

--- src/아무거나.java
+++ src/아무거나.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 public class 아무거나 {
     public static void main(String[] args) {
-
+        System.out.println("아무거나");
     }
 }
