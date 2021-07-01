92056f8899d00e45e05ed27ab0e7048b3f01e0e9
jHoon0223 <aleejaehun@naver.com>
2021-07-01 16:39:00 +0900

    210707 : BufferedReader / Writer practice

--- src/아무거나.java
+++ src/아무거나.java
@@ -1,5 +1,19 @@
+import java.io.*;
+import java.nio.Buffer;
+
 public class 아무거나 {
-    public static void main(String[] args) {
-        System.out.println("아무거나");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+        br.close();
+
+        //System.out.println(a + "," + b);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.append(a + "," + b);
+        bw.flush();
+        bw.close();
     }
 }
