b201142e266c9d74867568585d04f1c49b012d8a
jHoon0223 <aleejaehun@naver.com>
2021-07-04 17:56:20 +0900

    210704 : refresh

--- BAEKJOON_JAVA/src/BAEKJOON/B_10718.java
+++
@@ -1,8 +0,0 @@
-package BAEKJOON;
-
-public class B_10718 {
-    public static void main(String[] args) {
-        System.out.println("강한친구 대한육군");
-        System.out.println("강한친구 대한육군");
-    }
-}
