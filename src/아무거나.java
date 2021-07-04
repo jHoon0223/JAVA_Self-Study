166155ba2c5f88648c61cd36a3a5930d1343250e
jHoon0223 <aleejaehun@naver.com>
2021-07-04 21:40:03 +0900

    210704 : StringTokenizer

--- src/아무거나.java
+++ src/아무거나.java
@@ -1,12 +1,16 @@
 import java.io.*;
 import java.nio.Buffer;
+import java.util.StringTokenizer;
 
 public class 아무거나 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
+        /*String[] str = br.readLine().split(" ");
         int a = Integer.parseInt(str[0]);
-        int b = Integer.parseInt(str[1]);
+        int b = Integer.parseInt(str[1]);*/
+        StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(stk.nextToken());
+        int b = Integer.parseInt(stk.nextToken());
         br.close();
 
         //System.out.println(a + "," + b);
